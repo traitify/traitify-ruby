dbd504bf4cb16a73e9a404d8a0a65deb155a4505
sam199006 <sam@woofound.com>
2014-05-28 11:30:33 -0400

    added compatible_work_env

--- lib/traitify/personality_blends.rb
+++ lib/traitify/personality_blends.rb
@@ -1,14 +1,17 @@
 module Traitify
   class PersonalityBlend
-    attr_accessor :personality_type1, :personality_type2, :name, :description, :compliments, :conflicts
+    attr_accessor :personality_type1, :personality_type2, :name, :description, :compliments, :conflicts, :compatible_work_environments_1, :compatible_work_environments_2, :compatible_work_environments_3, :compatible_work_environments_4
 
     def initialize(options = {})
-      self.personality_type1 = options[:personality_type1]
-      self.personality_type2 = options[:personality_type2]
-      self.name              = options[:name]
-      self.description       = options[:description]
-      self.compliments       = options[:compliments]
-      self.conflicts         = options[:conflicts]
+      self.personality_type1              = options[:personality_type1]
+      self.personality_type2              = options[:personality_type2]
+      self.name                           = options[:name]
+      self.description                    = options[:description]
+      self.compliments                    = options[:compliments]
+      self.compatible_work_environments_1 = options[:compatible_work_environments_1]
+      self.compatible_work_environments_2 = options[:compatible_work_environments_2]
+      self.compatible_work_environments_3 = options[:compatible_work_environments_3]
+      self.compatible_work_environments_4 = options[:compatible_work_environments_4]
     end
 
     def self.parse_json(personality_blend)
@@ -16,23 +19,31 @@ module Traitify
       personality_type2 = PersonalityType.parse_json(personality_blend["personality_type2"])
 
       Result.new(
-        personality_type1: personality_type1,
-        personality_type2: personality_type2,
-        name:              personality_blend["name"],
-        description:       personality_blend["description"],
-        compliments:       personality_blend["compliments"],
-        conflicts:         personality_blend["conflicts"]
+        personality_type1:                      personality_type1,
+        personality_type2:                      personality_type2,
+        name:                                   personality_blend["name"],
+        description:                            personality_blend["description"],
+        compliments:                            personality_blend["compliments"],
+        conflicts:                              personality_blend["conflicts"],
+        compatible_work_environments_1:         personality_blend["compatible_work_environments_1"],
+        compatible_work_environments_2:         personality_blend["compatible_work_environments_2"],
+        compatible_work_environments_3:         personality_blend["compatible_work_environments_3"],
+        compatible_work_environments_4:         personality_blend["compatible_work_environments_4"]
       )
     end
 
     def to_hash
       {
-        personality_type1: personality_type1.to_hash,
-        personality_type2: personality_type2.to_hash,
-        name:              name,
-        description:       description,
-        compliments:       compliments,
-        conflicts:         conflicts
+        personality_type1:              personality_type1.to_hash,
+        personality_type2:              personality_type2.to_hash,
+        name:                           name,
+        description:                    description,
+        compliments:                    compliments,
+        conflicts:                      conflicts,
+        compatible_work_environments_1: compatible_work_environments_1,
+        compatible_work_environments_2: compatible_work_environments_2,
+        compatible_work_environments_3: compatible_work_environments_3,
+        compatible_work_environments_4: compatible_work_environments_4
       }
     end
   end
