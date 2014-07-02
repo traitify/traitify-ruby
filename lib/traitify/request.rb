0d3e2909dea394bdbed6aeab5890b595f054d072
Hashrocket Workstation <dev@hashrocket.com>
2014-07-01 23:09:30 -0400

    Remove superfluous code from the Request module

--- lib/traitify/request.rb
+++ lib/traitify/request.rb
@@ -1,46 +1,18 @@
 module Traitify
   module Request
-    def get(path, options = {})
-      request(:get, path, options)
-    end
-
-    def post(path, options = {})
-      request(:post, path, options)
-    end
-
-    def put(path, options = {})
-      request(:put, path, options)
-    end
-
-    def delete(path, options = {})
-      request(:delete, path, options)
-    end
-
-    def last_response
-      @last_response
-    end
-
-    private
-    def request(method, path, options = {})
-      path = "#{self.api_version}#{path}"
-      connection_options = {}.merge!(url: self.api_host)
-
-      response = connection(connection_options).send(method) do |request|
-        request.body = options.to_json if options
-        case method
-        when :get
-          request.url(path)
-        when :post
-          request.url(path)
-        when :put
-          request.url(path)
-        when :delete
-          request.url(path)
-        end
+    def method_missing(meth, *args, &block)
+      if [:get, :post, :put, :delete].include? meth
+        request(meth, *args)
+      else
+        super
       end
+    end
 
-      @last_response = response
-      response.body
+    def request(method, path, options = {})
+      connection(url: api_host).send(method) do |request|
+        request.url [api_version, path].join
+        request.body = options.to_json if options
+      end.body
     end
   end
 end
