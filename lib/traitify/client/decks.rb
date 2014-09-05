22eacaeaf28d203b98b3dd1c1f41944c49c826bc
Tom Prats <tprats108@gmail.com>
2014-09-05 13:52:23 -0400

    Added image packs option to decks call

--- lib/traitify/client/decks.rb
+++ lib/traitify/client/decks.rb
@@ -1,8 +1,14 @@
 module Traitify
   class Client
     module Deck
-      def decks
-        get("/decks").collect { |deck| Hashie::Mash.new(deck) }
+      def decks(image_pack = nil)
+        image_pack ||= self.image_pack
+
+        response = image_pack ?
+          get("/decks?image_pack=#{image_pack}") :
+          get("/decks")
+
+        response.collect { |deck| Hashie::Mash.new(deck) }
       end
       alias_method :find_decks, :decks
     end
