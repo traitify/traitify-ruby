956b6a526ca8988ea3dd26ab6aa807861ec5883f
Johnny Winn <johnny+nurugger07@traitify.com>
2014-10-02 00:48:58 -0400

    Add method to retrieve raw trait results

--- lib/traitify/client/results.rb
+++ lib/traitify/client/results.rb
@@ -21,6 +21,13 @@ module Traitify
       end
       alias_method :find_assessment_personality_traits, :assessment_personality_traits
 
+      def raw_personality_traits(assessment_id)
+        get("/assessments/#{assessment_id}/personality_traits/raw").collect do |personality_trait|
+          Hashie::Mash.new(personality_trait)
+        end
+      end
+      alias_method :find_personality_traits, :raw_personality_traits
+
       def career_matches(assessment_id, number_of_matches=10)
         response = get("/assessments/#{assessment_id}/matches/careers?number_of_matches=#{number_of_matches}")
         response.collect { |career| Hashie::Mash.new(career) }
