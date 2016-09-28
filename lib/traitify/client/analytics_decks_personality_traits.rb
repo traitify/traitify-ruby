35c503a9042b5350a233c830704c53adc6212550
Carson Wright <carsonnwright@gmail.com>
2016-09-28 00:52:04 -0400

    added fixes for analytics endpoints where group and user were not available

--- lib/traitify/client/analytics_decks_personality_traits.rb
+++ lib/traitify/client/analytics_decks_personality_traits.rb
@@ -3,8 +3,20 @@ module Traitify
     class Client < Stack
       def root(args = nil)
         set_verb(:get)
-        
+
         add_path("/personality_traits")
+
+        self
+      end
+
+      def groups(params)
+        add_params({group_ids: params})
+
+        self
+      end
+
+      def profiles(params)
+        add_params({profile_ids: params})
 
         self
       end
--- lib/traitify/client/analytics_decks_personality_types.rb
+++ lib/traitify/client/analytics_decks_personality_types.rb
@@ -3,8 +3,19 @@ module Traitify
     class Client < Stack
       def root(args = nil)
         set_verb(:get)
-        
+
         add_path("/personality_types")
+
+        self
+      end
+
+      def groups(params)
+        add_params({group_ids: params})
+
+        self
+      end
+      def profiles(params)
+        add_params({profile_ids: params})
 
         self
       end
